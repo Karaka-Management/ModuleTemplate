4b5730baa52e445dd4f8080c37944f7c8c42865e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-21 15:25:11 +0100

    Update Controller.php

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7.4
  *
- * @package   Modules\ModuleTemplate
+ * @package   Modules\ModuleTemplate\Controller
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -20,7 +20,7 @@ use phpOMS\Module\WebInterface;
 /**
  * Controller description.
  *
- * @package Modules\ModuleTemplate
+ * @package Modules\ModuleTemplate\Controller
  * @license OMS License 1.0
  * @link    https://orange-management.org
  * @since   1.0.0
